269e2082aeee7abde5ac21ff42840fabab87c7a4
Yukun Zhang <yukun@yukuns-mbp.mynetworksettings.com>
2024-06-19 23:15:26 -0400

    block34 completed

--- server/db.js
+++ server/db.js
@@ -1,13 +1,10 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
 import chalk from 'chalk';
-import { v4 as uuidv4 } from 'uuid';
 
 dotenv.config();
 
-const client = new pg.Client(
-  process.env.DATABASE_URL || 'postgres://localhost/Reservation_Planner'
-);
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/Reservation_Planner');
 
 const createTables = async () => {
   await client.connect();
@@ -15,44 +12,119 @@ const createTables = async () => {
   const SQL = `
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-    DROP TABLE IF EXISTS Reservations; 
-    DROP TABLE IF EXISTS Customers;
-    DROP TABLE IF EXISTS Locations;
+    DROP TABLE IF EXISTS Reservation; 
+    DROP TABLE IF EXISTS Customer;
+    DROP TABLE IF EXISTS Restaurant;
 
-    CREATE TABLE Customers (
+    CREATE TABLE Customer (
         id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
         customer_name VARCHAR(255) NOT NULL
     );
 
-    INSERT INTO Customers (customer_name)
-    VALUES ('Mumu'), ('Lulu'), ('Momo');
-
-
-    CREATE TABLE Locations (
+    CREATE TABLE Restaurant (
         id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-        location_name VARCHAR(255) NOT NULL
+        restaurant_name VARCHAR(255) NOT NULL
     );
 
-    INSERT INTO Locations (location_name)
-    VALUES ('Naya'), ('KFC'), ('Daxi');
-
-
-    CREATE TABLE Reservations (
+    CREATE TABLE Reservation (
         id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-        date DATE DEFAULT now(),
+        date DATE NOT NULL,
         party_count INTEGER NOT NULL,
-        location_id UUID,
+        restaurant_id UUID,
         customer_id UUID,
-        FOREIGN KEY (location_id) REFERENCES Locations(id),
-        FOREIGN KEY (customer_id) REFERENCES Customers(id)
+        FOREIGN KEY (restaurant_id) REFERENCES Restaurant(id),
+        FOREIGN KEY (customer_id) REFERENCES Customer(id)
     );
-
-    INSERT INTO Reservations (party_count)
-    VALUES ('2'), ('4'),('8')
-
     `;
   await client.query(SQL);
-  console.log(chalk.green('database successfuly created && data seeded!!'));
+  console.log(chalk.green('database successfuly created!'));
 };
 
-export { client, createTables };
+const createCustomer = async (customer_name) => {
+  const SQL = `
+  INSERT INTO Customer (customer_name)
+  VALUES ('${customer_name}')
+  RETURNING *
+  `;
+  try {
+    const response = await client.query(SQL);
+    console.log(chalk.green('A customer successfuly created!'));
+    return response.rows;
+  } catch {
+    (error) => {
+      console.log(chalk.red(error));
+      return error;
+    };
+  }
+};
+
+const createRestaurant = async (restaurant_name) => {
+  const SQL = `
+  INSERT INTO Restaurant (restaurant_name)
+  VALUES ('${restaurant_name}')
+  RETURNING *
+  `;
+  const response = await client.query(SQL);
+  console.log(chalk.green('A restanrant successfuly created!'));
+  return response.rows;
+};
+
+const fetchCustomers = async () => {
+  const SQL = `
+  SELECT * FROM Customer;
+  `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const fetchRestaurants = async () => {
+  const SQL = `
+  SELECT * FROM Restaurant;
+  `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const fetchReservations = async () => {
+  const SQL = `
+  SELECT * FROM Reservation;
+  `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const createReservation = async (customer_id, restaurant_id, date, party_count) => {
+  const SQL = `
+  INSERT INTO Reservation(customer_id, restaurant_id, date, party_count)
+  VALUES ('${customer_id}', '${restaurant_id}', '${date}', ${party_count})
+  RETURNING *
+  `;
+  try {
+    const response = await client.query(SQL);
+    return response.rows;
+  } catch (err) {
+    console.log('error: ', err);
+    throw err;
+  }
+};
+
+const deleteReservation = async (customer_id, id) => {
+  const SQL = `
+  DELETE FROM Reservation
+  WHERE id = '${id}' AND customer_id = '${customer_id}'
+ `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+export {
+  client,
+  createTables,
+  createCustomer,
+  createRestaurant,
+  createReservation,
+  deleteReservation,
+  fetchCustomers,
+  fetchRestaurants,
+  fetchReservations,
+};
--- server/index.js
+++ server/index.js
@@ -2,7 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import chalk from 'chalk';
 import dotenv from 'dotenv';
-import { createTables } from './db.js';
+import {
+  createTables,
+  createCustomer,
+  createRestaurant,
+  createReservation,
+  deleteReservation,
+  fetchCustomers,
+  fetchRestaurants,
+  fetchReservations,
+} from './db.js';
 
 dotenv.config();
 
@@ -21,3 +30,66 @@ const startServer = async () => {
 };
 
 startServer();
+
+app.get('/api/customers', async (req, res) => {
+  try {
+    const customers = await fetchCustomers();
+    res.send(customers);
+  } catch {
+    (error) => console.log(chalk.red(error));
+  }
+});
+
+app.post('/api/createCustomer', async (req, res) => {
+  try {
+    const customer = await createCustomer(req.body.name);
+    res.send(customer);
+  } catch {
+    (error) => console.log(chalk.red(error));
+  }
+});
+
+app.post('/api/createRestaurant', async (req, res) => {
+  try {
+    const restaurant = await createRestaurant(req.body.name);
+    res.send(restaurant);
+  } catch {
+    (error) => console.log(chalk.red(error));
+  }
+});
+
+app.get('/api/restaurant', async (req, res) => {
+  try {
+    const restaurants = await fetchRestaurants();
+    res.send(restaurants);
+  } catch {
+    (error) => console.log(chalk.red(error));
+  }
+});
+
+app.get('/api/reservations', async (req, res) => {
+  try {
+    const reservations = await fetchReservations();
+    res.send(reservations);
+  } catch {
+    (error) => console.log(chalk.red(error));
+  }
+});
+
+app.post('/api/customers/:id/reservations', async (req, res) => {
+  try {
+    const createdReservation = await createReservation(req.params.id, req.body.restaurant_id, req.body.date, req.body.party_count);
+    res.status(201).send(createdReservation);
+  } catch (error) {
+    console.log(chalk.red(error));
+  }
+});
+
+app.delete('/api/customers/:customer_id/reservations/:id', async (req, res) => {
+  try {
+    const response = await deleteReservation(req.params.customer_id, req.params.id);
+    res.sendStatus(204);
+  } catch {
+    (error) => console.log(chalk.red(error));
+  }
+});
